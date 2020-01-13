8447df873914790851ec6afc61e05e0ed01d33c5
wangyancai666 <ycai@jk-sci.com>
2020-01-13 13:58:53 +0800

    Updates

---
+++ precache-manifest.fd3722199d9298e740400fadae605eab.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d44136f400a5e2564ac882342324008",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e11d84442e61d9b964e0",
+    "url": "./static/css/2.12dea35f.chunk.css"
+  },
+  {
+    "revision": "3e7c7112103dd9c01953",
+    "url": "./static/css/main.56acc658.chunk.css"
+  },
+  {
+    "revision": "e11d84442e61d9b964e0",
+    "url": "./static/js/2.49a404e2.chunk.js"
+  },
+  {
+    "revision": "47468e639f211358d23dad40e3cf79e7",
+    "url": "./static/js/2.49a404e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83cee07d44daf7a1d4db",
+    "url": "./static/js/3.05ca9175.chunk.js"
+  },
+  {
+    "revision": "b09861b01c1aa935afb9",
+    "url": "./static/js/4.f33b39d1.chunk.js"
+  },
+  {
+    "revision": "3e7c7112103dd9c01953",
+    "url": "./static/js/main.7a9b160b.chunk.js"
+  },
+  {
+    "revision": "f281d9476643a50285d8",
+    "url": "./static/js/runtime-main.55e72410.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3014a4a44d7445f1c382134ea44ae6df",
+    "url": "./static/media/iconfont.3014a4a4.woff"
+  },
+  {
+    "revision": "332eb0ea45f614e723d920de34ae1703",
+    "url": "./static/media/iconfont.332eb0ea.ttf"
+  },
+  {
+    "revision": "53b699c7b032b14a807b3ae3d0a97e05",
+    "url": "./static/media/iconfont.53b699c7.eot"
+  },
+  {
+    "revision": "fcfdbe8176084870d15e5fb9d06f1899",
+    "url": "./static/media/iconfont.fcfdbe81.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
