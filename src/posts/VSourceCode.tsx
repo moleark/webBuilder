836ec19774b48ef1e26b4212c681ccf10d725a64
wangyancai666 <ycai@jk-sci.com>
2020-05-28 15:38:15 +0800

    修改

--- src/posts/VSourceCode.tsx
+++ src/posts/VSourceCode.tsx
@@ -12,11 +12,9 @@ export class VSourceCode extends VPage<CPosts> {
     private page = observer((param: any) => {
         let { current } = this.controller;
         return <Page header={this.t('sourcecode')} headerClassName={consts.headerClass}  >
-            <div className="w-100 h-100">
+            <div className="w-100 h-100"  >
                 <textarea className="w-100 h-100">{current.content}</textarea>
             </div>
         </Page>;
     })
-
-
 }
