3bdeef1114613709b0c4f93e94579feb7a234941
unknown <W363@wlrc.com>
2020-07-07 17:23:32 +0800

    搜索与切换开关的调整

--- src/posts/VInformationPost.tsx
+++ src/posts/VInformationPost.tsx
@@ -16,28 +16,6 @@ export class VInformationPost extends VPage<CPosts> {
 		return <this.page />;
 	}
 
-	// private onMeAll = (evt: React.ChangeEvent<HTMLInputElement>) => {
-	// 	this.isMes = evt.currentTarget.value === 'me';
-	// 	this.controller.setMe(this.isMes)
-	// }
-
-	// private MeAllToggle() {
-	// 	let cnButton = ['btn', 'btn-outline-warning', 'btn-sm', 'text-nowrap'];
-	// 	return <div className="px-sm-2 d-flex align-items-center">
-	// 		<div className="btn-group btn-group-toggle" data-toggle="buttons">
-	// 			<label className={classNames(cnButton, { active: this.isMes })}>
-	// 				<input type="radio" name="options" value="me" defaultChecked={true} onChange={this.onMeAll} />
-	// 				<span className="d-inline d-sm-none">{this.t('me-sm')}</span>
-	// 				<span className="d-none d-sm-inline">{this.t('me')}</span>
-	// 			</label>
-	// 			<label className={classNames(cnButton, { active: !this.isMes })}>
-	// 				<input type="radio" name="options" value="all" defaultChecked={false} onChange={this.onMeAll} />
-	// 				<span className="d-inline d-sm-none">{this.t('all-sm')}</span>
-	// 				<span className="d-none d-sm-inline">{this.t('all')}</span>
-	// 			</label>
-	// 		</div>
-	// 	</div>
-	// }
 	private page = observer(() => {
 		let { informationpagePosts, informationsearchPostsKey, onScrollBottom } = this.controller;
 		let none = (
@@ -47,7 +25,6 @@ export class VInformationPost extends VPage<CPosts> {
 		);
 		let right = (
 			<div className="d-flex align-items-center mr-1">
-				{/* {this.MeAllToggle()} */}
 				<SearchBox size="sm" onSearch={(key: string) => informationsearchPostsKey(key, "")} placeholder={this.t('searchpost')} />
 			</div>
 		);
@@ -64,7 +41,6 @@ export class VInformationPost extends VPage<CPosts> {
 
 		if (!user) return;
 		let { image, caption, discription, author, emphasis, web, agent, assist, openweb } = item;
-		// console.log(item)
 		let divUser = user.id === author.id ?
 			<span className="text-warning">[自己]</span>
 			:
