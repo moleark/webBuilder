a4883461e53d954acbe18eb0a93cca0c912a0bd2
ligsh <system6@jkchemical.com>
2020-12-16 13:34:47 +0800

    ligsh, 样式修改

--- src/posts/VMain.tsx
+++ src/posts/VMain.tsx
@@ -195,16 +195,17 @@ export class VMain extends VPage<CPosts> {
                                 {showImport}
                             </div>
                             <div className="author">
-                                {sumHits && sumHits > 0 && <>阅读<b>{sumHits}</b>次</>}{<span className="px-1"></span>}{sumHits >= hits && hits > 0 && <>周<b>{hits}</b>次</>}
+                                {sumHits && sumHits > 0 && <>阅读<b>{sumHits}</b>次</>}
+                                {<span className="px-1"></span>}{sumHits >= hits && hits > 0 && <>周<b>{hits}</b>次</>}
                             </div>
                         </div>
                         <div className="small py-1 nowrap" style={{ overflow: "hidden" }}>
                             {(web + agent + assist + openweb) > 0 ? <span className=" text-muted">发布：</span> : <></>}
-                            {agent === 1 ? <span style={{ borderRadius: "15%/48%" }} className="bg-success mr-1 text-white px-1">{this.t('agent')}</span> : <></>}
-                            {assist === 1 ? <span style={{ borderRadius: "15%/48%" }} className="bg-warning mr-1 text-white px-1">{this.t('sales')}</span> : <></>}
-                            {openweb === 1 ? <span style={{ borderRadius: "15%/48%" }} className="bg-info mr-1 text-white px-1">{this.t('publicSite')}</span> : <></>}
-                            {web === 1 ? <span style={{ borderRadius: "15%/48%" }} className="bg-primary text-white px-1">{this.t('internationSite')}</span> : <></>}
-                            {bvweb === 1 ? <span style={{ borderRadius: "15%/48%" }} className="bg-primary text-white px-1">{this.t('BV网站')}</span> : <></>}
+                            {agent === 1 ? <span className="bg-success rounded-pill mr-1 text-white px-1">{this.t('agent')}</span> : <></>}
+                            {assist === 1 ? <span className="bg-warning rounded-pill mr-1 text-white px-1">{this.t('sales')}</span> : <></>}
+                            {openweb === 1 ? <span className="bg-info rounded-pill mr-1 text-white px-1">{this.t('publicSite')}</span> : <></>}
+                            {web === 1 ? <span className="bg-primary rounded-pill text-white px-1">{this.t('internationSite')}</span> : <></>}
+                            {bvweb === 1 ? <span className="bg-primary rounded-pill text-white px-1">{this.t('BV网站')}</span> : <></>}
                         </div>
                         <div>
                             {tagCatalogname}
